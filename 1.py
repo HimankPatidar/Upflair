e845334654a81f35f006c442c5c6c2049092bf54
Himank Patidar <patidarhimank005@gmail.com>
2024-04-22 16:05:20 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,29 @@
+##import keyword
+##print(keyword.kwlist)
+##print(len(keyword.kwlist))
+####print("Hello World")
+###Operators
+###Arithmaric Operator
+###// = floor division
+### ** = exponential (power)
+##
+###keywordkeyword
+
+
+##Relational Operator(>,<,==,!=, <=, >=)
+##assignment operator - (=, +=, -=, *=
+##+= ------ x+=3 -------- 
+#### Logical Operator(and,or,not)
+
+##bitwise Operator ----- (&, ||,^, .....)
+##Identity Operator ----- (is, isnot)
+## Membership Operators -------- (in, not in)
+
+
+
+#DataType
+
+##Sequence Data Type :- list, string, Tuple,
+####list - []   mutable
+####tuple = () immutable
+####set = {} immutable
