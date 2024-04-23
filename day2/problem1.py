5f620c73968c0ce82ee3387b194d72bf89c3904b
HimankPatidar <patidarhimank005@gmail.com>
2024-04-23 22:13:34 +0530

    Add code examples for data types in Python

---
+++ day2/problem1.py
@@ -0,0 +1,20 @@
+a="Ram"
+b="and"
+c="Shyam"
+
+print(a + " " + b + " " + c)
+print(a,b,c)
+
+
+#problem 2
+
+a=51
+b=34
+c=23
+
+if a>=b and a>=c :
+    print("A is maximum :", a)
+elif b>=a and b>=c:
+    print("B is maximum :", b)
+else:
+    print("C is maximum :" ,c)
