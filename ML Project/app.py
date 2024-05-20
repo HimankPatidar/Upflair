68150378aa0608435c0ecf14f5a4753f98f536e3
HimankPatidar <patidarhimank005@gmail.com>
2024-05-20 10:33:26 +0530

    update

--- ML Project/app.py
+++ ML Project/app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
+import joblib
+model = joblib.load("rfmodel.lb")
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
@@ -10,11 +14,24 @@ def input():
     return render_template("input.html")
 
 
-@app.route("/prediction")
+@app.route('/prediction', methods = ['GET','POST'])
 def prediction() :
-    return render_template("prediction.html")
+       
+           if request.method == 'POST':
+                region = request.form['region']
+                children = request.form['children']
+                smoker = request.form['smoker']
+                age = request.form['age']
+                bmi = request.form['bmi']
+                gender = request.form['gender']
+                user_data = [region,children,smoker,age,bmi,gender]
+                # model.predict(x_variable )
+                return user_data
+
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 #print(__name__)
+
+
